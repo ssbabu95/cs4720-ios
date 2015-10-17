a779f38796d46b18ca661882a46ec976d5028237
ssbabu95 <ssb9qu@virginia.edu>
2015-10-17 11:21:56 -0400

    Initial Commit

---
+++ GroceryShopperHelper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GroceryShopperHelper
+//
+//  Created by Sumeet Babu on 10/17/15.
+//  Copyright © 2015 Sumeet Babu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
