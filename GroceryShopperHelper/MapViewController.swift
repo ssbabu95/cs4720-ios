76039583261f232959d48096fb5addd56ecb6e99
ssbabu95 <ssb9qu@virginia.edu>
2015-11-01 22:29:59 -0500

    USEFUL GPS THING

---
+++ GroceryShopperHelper/MapViewController.swift
@@ -0,0 +1,73 @@
+//
+//  MapViewController.swift
+//  GroceryShopperHelper
+//
+//  Created by Sumeet on 11/1/15.
+//  Copyright © 2015 Sumeet Babu. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+    
+    var lat: Double!
+    var lon: Double!
+
+    @IBOutlet weak var mapView: MKMapView!
+     var matchingItems: [MKMapItem] = [MKMapItem]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let initialLocation = CLLocationCoordinate2D(latitude: lat, longitude: lon)
+        let region = MKCoordinateRegionMakeWithDistance(
+            initialLocation, 2000, 2000)
+        
+        mapView.setRegion(region, animated: true)
+        performSearch()
+
+    }
+    
+    
+    func performSearch() {
+        
+        matchingItems.removeAll()
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = CLLocationCoordinate2D(latitude: lat, longitude: lon)
+        annotation.title = "Current Location"
+        self.mapView.addAnnotation(annotation)
+        
+        let request = MKLocalSearchRequest()
+        request.naturalLanguageQuery = "Grocery"
+        request.region = mapView.region
+        
+        let search = MKLocalSearch(request: request)
+        
+        search.startWithCompletionHandler({(response: MKLocalSearchResponse?, error: NSError?) in
+            
+            if error != nil {
+                print("Error occured in search: \(error!.localizedDescription)")
+            } else if response!.mapItems.count == 0 {
+               print("No matches found")
+            } else {
+                print("Matches found")
+                
+                for item in response!.mapItems {
+                    print("Name = \(item.name)")
+                    print("Phone = \(item.phoneNumber)")
+                    
+                    self.matchingItems.append(item as MKMapItem)
+                    print("Matching items = \(self.matchingItems.count)")
+                    
+                    let annotation = MKPointAnnotation()
+                    annotation.coordinate = item.placemark.coordinate
+                    annotation.title = item.name
+                    self.mapView.addAnnotation(annotation)
+                }
+            }
+        })
+    }
+    
+
+
+}
--- GroceryShopperHelper/ViewController.swift
+++ GroceryShopperHelper/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 import MessageUI
 import AVFoundation
+import CoreLocation
 
-class ViewController: UIViewController, MFMailComposeViewControllerDelegate {
+class ViewController: UIViewController, MFMailComposeViewControllerDelegate, CLLocationManagerDelegate {
+    
+    var locationManager: CLLocationManager?
+    var lat: Double?
+    var lon: Double?
     
     @IBOutlet weak var nameText: UITextField!
+    
     @IBOutlet weak var showName: UILabel!
 
     @IBOutlet weak var createButton: UIButton!
@@ -24,6 +30,32 @@ class ViewController: UIViewController, MFMailComposeViewControllerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if CLLocationManager.locationServicesEnabled(){
+            
+            switch CLLocationManager.authorizationStatus(){
+            case .AuthorizedAlways:
+                createLocationManager(startImmediately: true)
+            case .AuthorizedWhenInUse:
+                createLocationManager(startImmediately: true)
+            case .Denied:
+                displayAlertWithTitle("Not Determined",
+                    message: "Location services are not allowed for this app")
+            case .NotDetermined:
+                createLocationManager(startImmediately: false)
+                if let manager = self.locationManager{
+                    manager.requestWhenInUseAuthorization()
+                }
+            case .Restricted:
+                displayAlertWithTitle("Restricted",
+                    message: "Location services are not allowed for this app")
+            }
+            
+            
+        } else {
+            print("Location services are not enabled")
+        }
+
         self.showName.text = "";
         createButton.titleLabel?.adjustsFontSizeToFitWidth = true;
         shareButton.titleLabel?.adjustsFontSizeToFitWidth = true;
@@ -33,11 +65,72 @@ class ViewController: UIViewController, MFMailComposeViewControllerDelegate {
         
 
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        if locations.count == 0{
+            print("?")
+            return
+        }
+        
+        let newLocation = locations[0]
+        
+        print("Latitude = \(newLocation.coordinate.latitude)")
+        print("Longitude = \(newLocation.coordinate.longitude)")
+        lat = newLocation.coordinate.latitude
+        lon = newLocation.coordinate.longitude
+        
     }
+    
+    func locationManager(manager: CLLocationManager,
+        didFailWithError error: NSError){
+            print("Location manager failed with error = \(error)")
+    }
+    
+    func locationManager(manager: CLLocationManager,
+        didChangeAuthorizationStatus status: CLAuthorizationStatus){
+            
+            print("The authorization status of location services is changed to: ", terminator: "")
+            
+            switch CLLocationManager.authorizationStatus(){
+            case .AuthorizedAlways:
+                print("Authorized")
+            case .AuthorizedWhenInUse:
+                print("Authorized when in use")
+            case .Denied:
+                print("Denied")
+            case .NotDetermined:
+                print("Not determined")
+            case .Restricted:
+                print("Restricted")
+            }
+            
+    }
+    
+    func displayAlertWithTitle(title: String, message: String){
+        let controller = UIAlertController(title: title,
+            message: message,
+            preferredStyle: .Alert)
+        
+        controller.addAction(UIAlertAction(title: "OK",
+            style: .Default,
+            handler: nil))
+        
+        presentViewController(controller, animated: true, completion: nil)
+        
+    }
+    
+    func createLocationManager(startImmediately startImmediately: Bool){
+        locationManager = CLLocationManager()
+        if let manager = locationManager{
+            print("Successfully created the location manager")
+            manager.delegate = self
+            if startImmediately{
+                manager.startUpdatingLocation()
+            }
+        }
+    }
+    
     
     @IBAction func changeName(sender : AnyObject) {
         showName.text = "Grocery list for " + nameText.text! + " will be created.";
@@ -45,18 +138,7 @@ class ViewController: UIViewController, MFMailComposeViewControllerDelegate {
         
     }
     
-    @IBAction func pinchAction(sender: UIPinchGestureRecognizer) {
-        nameText.text = "Walmart";
-    }
-    
     @IBAction func shareAction(sender: AnyObject) {
-        //let email = MFMailComposeViewController()
-        //email.setSubject(showName.text!)
-        //email.mailComposeDelegate = self
-        //self.presentViewController(email, animated: true, completion: {})
-        //let store = showName.text!
-        //let activityViewController = UIActivityViewController(activityItems: [store as NSString], applicationActivities: nil)
-        //presentViewController(activityViewController, animated: true, completion :{})
         let mailComposerVC = MFMailComposeViewController()
         mailComposerVC.mailComposeDelegate = self
         mailComposerVC.setSubject(showName.text!)
@@ -79,6 +161,16 @@ class ViewController: UIViewController, MFMailComposeViewControllerDelegate {
     }
     func DismissKeyboard(){
         view.endEditing(true)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        if (segue.identifier == "mapSeg") {
+            if let svc = segue.destinationViewController as? MapViewController {
+                svc.lat = lat
+                svc.lon = lon
+                
+            }
+        }
     }
 
 }
